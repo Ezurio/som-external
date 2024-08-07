430bc2ff687c102ae1fced339ee5d1489c19476b
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2024-08-07 15:24:32 -0400

    Added support for secure SD card build
    Non secure SD builds now operate similar to flash
    
    PROD-10027

--- board/post_build_common_60.sh
+++ board/post_build_common_60.sh
@@ -214,8 +214,6 @@ case "${BUILD_TYPE}" in
 		;;
 esac
 
-if [ "${BUILD_TYPE}" != ig60 ]; then
-
 if grep -qF 'CONFIG_SIGNED_IMAGES=y' "${BUILD_DIR}"/swupdate*/include/config/auto.conf; then
 	mkdir -p "${TARGET_DIR}"/etc/swupdate/conf.d
 	if grep -qF 'CONFIG_SIGALG_CMS=y' "${BUILD_DIR}"/swupdate*/include/config/auto.conf; then
@@ -296,8 +294,6 @@ DTB="$(sed -n 's/^BR2_LINUX_KERNEL_INTREE_DTS_NAME="\(.*\)"$/\1/p' "${BR2_CONFIG
 DTB="${DTB##*/}"
 
 sed -i "s/at91-dvk_som60/${DTB}/g" "${BINARIES_DIR}/kernel.its"
-
-fi
 
 case "${BUILD_TYPE}" in
 	wb50n*) SOM=wb50n ;;
