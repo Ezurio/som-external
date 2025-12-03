1a6f5fd24999ea5c2dc059f710bfe570dea2d902
Chris Trowbridge <chris.trowbridge@ezurio.com>
2025-12-03 09:02:35 -0500

    feat: Support AWS KMS signing via PKCS#11
    This change introduces a new feature to support signing images via the AWS Key
    Management Service (KMS). The main benefit here is that the private key material
    does not need to be stored locally on the build machine and is never exposed
    outside of AWS KMS.
    
    Two new packages are introduced here: `pkcs11-provider` and `aws-kms-pkcs11`.
    
    `pkcs11-provider` is an OpenSSL provider which enables the use of PKCS#11 APIs
    for cryptographic operations.
    
    `aws-kms-pkcs11` is a project which exposes a PKCS#11 API for interfacing with
    AWS KMS.
    
    When used together, images can be signed with a key stored in AWS KMS.
    
    Newly introduced environment variables:
    - `AWS_KMS_SIGNING`: Set to `y` to enable AWS KMS signing logic
    - `AWS_KMS_SIGNING_PKCS11_PUBLIC_CERT_PATH`: The path to the local copy of the
    public key which corresponds to the signing public key. This is also assumed to
    be the "keys" directory.
    - `AWS_KMS_PKCS11_SLOT_LABEL`: Target AWS KMS key alias (or label)
    - `AWS_KMS_PKCS11_KMS_KEY_ID`: Target AWS KMS key ID
    - `AWS_KMS_PKCS11_AWS_REGION`: Target AWS KMS key AWS region
    
    Refs: PROD-45023

---
+++ package-3rd-party/aws-sdk-cpp/aws-sdk-cpp.mk
@@ -0,0 +1,26 @@
+################################################################################
+# aws-sdk-cpp
+################################################################################
+AWS_SDK_CPP_VERSION = 1.11.701
+AWS_SDK_CPP_SITE_METHOD = git
+AWS_SDK_CPP_SITE = https://github.com/aws/aws-sdk-cpp.git
+HOST_AWS_SDK_CPP_DEPENDENCIES = host-cmake host-openssl host-util-linux
+AWS_SDK_CPP_LICENSE = Apache-2.0
+AWS_SDK_CPP_LICENSE_FILES = LICENSE
+AWS_SDK_CPP_SUPPORTS_IN_SOURCE_BUILD = NO
+
+HOST_AWS_SDK_CPP_CONF_OPTS += \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DBUILD_ONLY="kms;acm-pca" \
+	-DENABLE_TESTING=OFF \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DUSE_CRT_HTTP_CLIENT=ON
+
+define AWS_SDK_CPP_CMAKE_MOVE_HOOK
+	cd $(@D) && ./prefetch_crt_dependency.sh
+    mkdir $(@D)/build
+endef
+
+HOST_AWS_SDK_CPP_PRE_CONFIGURE_HOOKS += AWS_SDK_CPP_CMAKE_MOVE_HOOK
+
+$(eval $(host-cmake-package))
