484d4ac32a7cab39b6d12462969e09a660dd0f87
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2026-04-05 14:50:12 -0400

    carbon_am62l: Added support for am62l eMMC flavor
    - Workaround for CAN crash
    - Improved set-mode
    
    PROD-27490

--- board/post_build_common.sh
+++ board/post_build_common.sh
@@ -303,10 +303,17 @@ ENV_SIZE=$(sed -rn 's,^CONFIG_ENV_SIZE=(.*),\1,p' "${BUILD_DIR}/uboot-${UBOOT_VE
 ENV_OFFSET=$(sed -rn 's,^CONFIG_ENV_OFFSET=(.*),\1,p' "${BUILD_DIR}/uboot-${UBOOT_VER}/.config")
 TEXT_BASE=$(sed -rn 's,^CONFIG_TEXT_BASE=(.*),\1,p' "${BUILD_DIR}/uboot-${UBOOT_VER}/.config")
 
+ENV_OFFSET_EMMC=$(fdtget "${BUILD_DIR}/uboot-${UBOOT_VER}/u-boot.dtb" /config u-boot,mmc-env-offset)
+if [ -n "${ENV_OFFSET_EMMC}" ] ; then
+	ENV_OFFSET_EMMC=$(printf '0x%x' "${ENV_OFFSET_EMMC}")
+else
+	ENV_OFFSET_EMMC=${ENV_OFFSET}
+fi
+
 create_fw_env_emmc_sd() {
 	emmc=$(sed -rn 's,^BR2_SUMMIT_EMMC_DEVICE=([0-9]+).*,\1,p' "${BR2_CONFIG}")
-	echo "/dev/mmcblk${emmc}boot0 ${ENV_OFFSET} ${ENV_SIZE}" > "${TARGET_DIR}/etc/fw_env_emmc-a.config"
-	echo "/dev/mmcblk${emmc}boot1 ${ENV_OFFSET} ${ENV_SIZE}" > "${TARGET_DIR}/etc/fw_env_emmc-b.config"
+	echo "/dev/mmcblk${emmc}boot0 ${ENV_OFFSET_EMMC} ${ENV_SIZE}" > "${TARGET_DIR}/etc/fw_env_emmc-a.config"
+	echo "/dev/mmcblk${emmc}boot1 ${ENV_OFFSET_EMMC} ${ENV_SIZE}" > "${TARGET_DIR}/etc/fw_env_emmc-b.config"
 	echo "/boot/uboot.env 0 ${ENV_SIZE}" > "${TARGET_DIR}/etc/fw_env_sd.config"
 }
 
