ae30bf283e296b9b9a78bc0f013eedc553d5fc82
Boris Krasnovskiy <boris.krasnovskiy@ezurio.com>
2026-06-30 08:13:18 -0400

    carbon_am62l: Fixed NAND update
    PROD-41857

--- board/post_build_common.sh
+++ board/post_build_common.sh
@@ -431,6 +431,7 @@ case "${BUILD_TYPE}" in
 
 		if grep -qF "BR2_PACKAGE_MTD=y" "${BR2_CONFIG}"; then
 			create_fw_env_flash
+			ln -rsf "${CSCRIPT_DIR}/erase_data.sh" "${BINARIES_DIR}/erase_data.sh"
 		fi
 
 		export UBOOT_LOADADDRESS=0x82000000
